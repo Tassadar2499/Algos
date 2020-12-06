7527b73b072d0ba59a5c54bf60c53f42c363addf
Tassadar2499 <markevich.roma@gmail.com>
2020-12-06 20:34:12 +0500

    Binary tree

---
+++ Default/BinaryTree/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BinaryTree
+{
+	public class BinaryTree
+	{
+		public class BinaryNode
+		{
+			public readonly int Value;
+			public BinaryNode Left { get; set; }
+			public BinaryNode Right { get; set; }
+
+			public BinaryNode(int value)
+			{
+				Value = value;
+			}
+		}
+
+		public BinaryNode RootNode { get; private set; }
+
+		private void Add(BinaryNode node, BinaryNode currentNode)
+		{
+			if (RootNode == null)
+				RootNode = node;
+			else
+			{
+				if (currentNode.Value >= node.Value)
+				{
+					if (currentNode.Left == null)
+						currentNode.Left = node;
+					else
+						Add(node, currentNode.Left);
+				}
+				else
+				{
+					if (currentNode.Right == null)
+						currentNode.Right = node;
+					else
+						Add(node, currentNode.Right);
+				}
+			}
+		}
+
+		public void Add(int value)
+		{
+			Add(new BinaryNode(value), RootNode);
+		}
+
+		// прямой обход (CLR - center, left, right)
+		public void CLR(BinaryNode node)
+		{
+			if (node != null)
+			{
+				Console.WriteLine(node.Value.ToString());
+				CLR(node.Left);
+				CLR(node.Right);
+			}
+		}
+		// Внутренний обход (LCR - left, center, right) 
+		public void LCR(BinaryNode node)
+		{
+			if (node != null)
+			{
+				LCR(node.Left);
+				Console.WriteLine(node.Value.ToString());
+				LCR(node.Right);
+			}
+		}
+		// Обратный обход (RCL - left, right, center)
+		public void RCL(BinaryNode node)
+		{
+			if (node != null)
+			{
+				RCL(node.Left);
+				RCL(node.Right);
+				Console.WriteLine(node.Value.ToString());
+			}
+		}
+	}
+}
---
+++ Default/BinaryTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("kek");
+			var binTree = new BinaryTree();
+			binTree.Add(10);
+			binTree.Add(6);
+			binTree.Add(5);
+			binTree.Add(8);
+			binTree.Add(7);
+			binTree.Add(14);
+			binTree.Add(12);
+			binTree.Add(15);
+
+			Console.WriteLine("Прямой");
+			binTree.CLR(binTree.RootNode);
+			Console.WriteLine("Внутренний");
+			binTree.LCR(binTree.RootNode);
+			Console.WriteLine("Обратный");
+			binTree.RCL(binTree.RootNode);
+
+			Console.ReadKey();
+		}
+	}
+}
