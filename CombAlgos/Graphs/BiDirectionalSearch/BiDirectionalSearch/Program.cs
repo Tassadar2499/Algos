d3a5809fb81020c74446012d3ce0ce37112f8619
tassadar2499 <markevich.roma@gmail.com>
2021-10-14 21:21:56 +0300

    add bidirectionalsearch

---
+++ CombAlgos/Graphs/BiDirectionalSearch/BiDirectionalSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BiDirectionalSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
